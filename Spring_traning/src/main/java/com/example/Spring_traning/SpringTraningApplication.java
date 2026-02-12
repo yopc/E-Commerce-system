6bb1b578251e745947f895fa1194e7cba4e7817f
yosephNative <safaridagm@gmail.com>
2026-02-12 05:40:11 +0300

    Spring boot training Ecomerce full stack application training

---
+++ Spring_traning/src/main/java/com/example/Spring_traning/SpringTraningApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_traning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTraningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTraningApplication.class, args);
+	}
+
+}
---
+++ Spring_traning/src/test/java/com/example/Spring_traning/SpringTraningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_traning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTraningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
